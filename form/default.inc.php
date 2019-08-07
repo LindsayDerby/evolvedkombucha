92b2ab6f35efd44fcb3181696e384eaa2a9bfe39
Lindsay Derby <lindsay.derby@ufl.edu>
2019-08-07 16:42:14 -0400

    Added lightbox and animations.

--- form/default.inc.php
+++ form/default.inc.php
@@ -1,26 +1,135 @@
 <style type="text/css">
 * {
-margin: 0;
-padding: 0;
-border: 0;
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-family: 'Barlow Condensed', sans-serif;
 }
 
-body {
-  background-color: #f7f7f7;
+
+body{
+  background: #f7f7f7;
+	overflow-x: hidden;
 }
+
 
 .container {
-  background-color: #F7F7F7;
-  color: black;
-  padding: 80px 30px 40px 30px;
-  max-width: 500px;
-  margin-left: 50px;
+  max-width: 100%;
+  background: #f7f7f7;
+
 }
+
+section {
+  margin-bottom: 40px;
+  background: #f7f7f7;
+
+}
+
+main {
+  padding-top: 102px;
+  background: #f7f7f7;
+  padding-bottom: 40px;
+}
+
+main .container {
+  background-color: white;
+  padding: 20px;
+  background: #f7f7f7;
+
+}
+
+html {
+    position: relative;
+    min-height: 100%;
+}
+
+h1 {
+  font-family: 'Barlow Condensed', sans-serif;
+  color: black;
+  font-size: 30px;
+  margin-bottom: 15px;
+}
+
+h2 {
+  font-family: 'Barlow Condensed', sans-serif;
+  color: black;
+  font-size: 20px;
+  margin-bottom: 0px;
+	margin-top: 5px;
+	font-weight: 500;
+}
+
+
+p {
+  font-family: 'Lato', sans-serif;
+  color: black;
+  font-size: 14px;
+  line-height: 20px;
+  margin-bottom: 20px;
+}
+
+
+/*===================
+*
+* Footer
+*
+=====================*/
+
+
+footer {
+ position: absolute;
+ bottom: 0;
+ width: 100%;
+ height: 30px;
+ background-color: #000;
+ color: white;
+ padding: 20px;
+ display: inline-block;
+ left: 0;
+}
+
+#footer-text {
+  color: white;
+  text-align: left;
+}
+
+.footer-text, .footer-icons {
+	width: 40%;
+	display: inline-block;
+}
+
+.footer-icons {
+float: right;
+	text-align: right;
+	margin-right: 60px;
+}
+
+.footer-icons img:hover {
+	opacity: .5;
+}
+
+
+/*===================
+*
+* Header
+*
+=====================*/
 
 header {
   background: #F7F7F7;
   padding: 20px 0px;
+  position: fixed;
+  width: 100%;
+  margin-right: 50px;
+  margin-bottom: 50px;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center top;
+  background-size: auto;
+	z-index: 9999;
 }
+
+
 
 .logo {
   float: left;
@@ -28,6 +137,7 @@ header {
   display: inline;
   padding-left: 30px;
 }
+
 
 nav {
     float: right;
@@ -52,7 +162,7 @@ nav a:active {
   padding: 0px 20px;
   font-family: 'Barlow Condensed', sans-serif;
   text-transform: uppercase;
-  font-weight: bold;
+  font-weight: 900;
 }
 
 nav a:hover {
@@ -66,43 +176,38 @@ nav a:hover {
 }
 
 
-h1 {
-  font-family: 'Barlow Condensed', sans-serif;
-  color: black;
-  font-size: 30px;
-  margin-bottom: 15px;
-}
 
-h1::before {
-  content: url(http://evolvedkombucha.com/icon.png);
-}
-
-p{
-  font-family: 'Lato', sans-serif;
-  color: black;
-  font-size: 14px;
-  line-height: 20px;
-  margin-bottom: 20px;
-}
-
-p::selection {
-  background-color: #fbf4bd;
-}
 </style>
 
-<header>
-    <img src="images/logo.png" class="logo"/>
-    <nav>
-      <ul>
-        <li><a href="index.html">Home</a></li>
-        <li><a href="about.html">About</a></li>
-        <li><a href="products.html">Products</a></li>
-        <li><a href="contact.html">Contact</a></li>
-      </ul>
-    </nav>
-</header>
+    <header>
+      <a href="index.html"><img src="images/logo-horizontal.png" class="logo" alt="Evolved Kombucha Logo" title="logo"/></a>
+        <nav>
+          <ul class="top-menu">
+            <li><a href="/">Home</a></li>
+            <li><a href="about">About</a></li>
+            <li><a href="products">Products</a></li>
+            <li><a href="contact">Contact</a></li>
+          </ul>
+        </nav>
+    </header>
 
 <div class="container">
   <h1>OOPS! SOMETHING WENT WRONG.</h1>
   <p>Please refresh the page and try again.</p>
 </div>
+
+    <footer>
+    <div id="footer">
+      	<div class="footer-text">
+		<p id="footer-text">Evolved Kombucha</p>
+		</div>
+
+		<div class="footer-icons">
+		<a href="#"><img src="images/facebook.png"  alt="Facebook" style="height: 15px; padding-left: 10px;"></a>
+		<a href="#"><img src="images/instagram.png"  alt="Instagram" style="height: 13px;  padding-left: 10px;"></a>
+		<a href="#"><img src="images/youtube.png"  alt ="Youtube" style="height: 13px;  padding-left: 10px;"></a>
+        <a href="contact.html"><img src="images/email.png" alt="Email" style="height: 12px;  padding-left: 10px;"></a>
+		</div>
+
+    </div>
+    </footer>
